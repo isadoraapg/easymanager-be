d37bdfd187f32a566fa332af3cd5ad302b7af1b8
Isadora Peixoto <isadorapgalego@gmail.com>
2025-06-23 23:10:54 -0300

    conectando web

--- config/database.js
+++ config/database.js
@@ -2,16 +2,23 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
+  process.env.DB_NAME,  // Nome do banco de dados
+  process.env.DB_USER,  // Usuário do banco
+  process.env.DB_PASS,  // Senha do banco
   {
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
+    host: process.env.DB_HOST,  // Endereço do banco fornecido pelo Render
+    port: process.env.DB_PORT,  // Porta do banco (provavelmente 5432)
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false  // Em ambientes de produção, isso pode ser ajustado conforme a segurança exigida
+      }
+    },
     logging: false,
     define: {
-      timestamps: false,
-      underscored: false
+      timestamps: true,
+      underscored: false, // ou true, conforme sua preferência
     }
   }
 );
