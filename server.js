dd9fab8e85f150fd2f522f8434c6abff16fa6281
Isadora Peixoto <isadorapgalego@gmail.com>
2025-06-24 20:01:44 -0300

    ajuste teste build2

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const sequelize = require('./config/database');
-const { Produto } = require('./models');
+
 const fs = require('fs');
 const mysql = require('mysql2/promise');
 
